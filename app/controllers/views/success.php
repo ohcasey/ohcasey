adce537625c5b132a8c303459dfd53602d01a3cf
atanikov <atanikov@gmail.com>
2015-09-06 11:06:38 +0300

    uccess.php корректный путь к analyticstracking.php

--- app/controllers/views/success.php
+++ app/controllers/views/success.php
@@ -17,7 +17,7 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-	<?php include_once("analyticstracking.php") ?>
+	<?php include_once("templates/analyticstracking.php") ?>
 	<!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter32242774 = new Ya.Metrika({ id:32242774, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/32242774" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 	<div class="main_container">
 		<div class="line_block top"></div>
