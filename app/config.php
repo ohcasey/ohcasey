c3011f3e7ad890614d760546ec31b26ec12d98ae
atanikov <atanikov@gmail.com>
2015-09-21 16:24:45 +0300

    тест фона для iphone6 от Алены

--- app/config.php
+++ app/config.php
@@ -1289,6 +1289,9 @@ iphone4_black_crystal_case.png
 					"small"=>"tufli_valentino_r82.png","chechs"=>array(16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)),
 
 					//iphone6
+					array("big"=>"6-6+/testtest.png",
+					"small"=>"bembi_r82.png","chechs"=>array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)),
+					
 					array("big"=>"6-6+/bembi_i6-6+.png",
 					"small"=>"bembi_r82.png","chechs"=>array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)),
 					array("big"=>"6-6+/devochka_i6-6+.png",
