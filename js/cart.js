9b325373f7855a9e549b03352ab3c02ad9cac7d3
atanikov <atanikov@gmail.com>
2015-09-28 13:30:13 +0300

    заглушки от старых браузеров, мобильников и в корзину

--- js/cart.js
+++ js/cart.js
@@ -2,32 +2,41 @@ $(document).on("click",".checkbox_prev_input",function(){
 	$(this).parent().find("input").click();
 });
 
+
+
 function preparing_html() {
-	$("body").css("min-height","768px");
+	//$("body").css("min-height","768px");
 	var html_width = $("body").width();
 	var html_height = $(document).height();
 	$(".main_container").css("height", html_height+"px");
 	$(".main_container, #header").css("width", html_width+"px");
 	$(".header_menu__item").css({"width": ((html_width - $("#header-logo").width() - 20*5)/6) +"px", "visibility": "visible"});
 	
+	$(".center-cart_block").css("margin-left", ($("#left").width() + 50)+"px");
+	//$(".right_cart").css("width",  (html_width - $("#left").width() - $(".center-cart_block").width() - 70) +"px" );
 	to_down_of_page();
 
-
+	if (html_width>1979) {
+		$("#order_form").css("height",(html_height-70-130-60)+"px");
+	}else{
+		$("#order_form").css("height",(html_height-70-70-60)+"px");
+	}
+	
 	var count = $(".cart_item_block").length;
-
-	if (((count>2) && (html_width<1200))||((count>1) && (html_width>=1200))) {
+	
+	$('.overflow_form, .city_list-sdec').perfectScrollbar({wheelSpeed: 30, wheelPropagation: false, minScrollbarLength: 1, suppressScrollX: false});
+	if (((count>1) && (html_width<1200))||((count>1) && (html_width>=1200))) {
 		$('.cart_items_block').perfectScrollbar({wheelSpeed: 30, wheelPropagation: false, minScrollbarLength: 1, suppressScrollX: false});
 	}else{
-		$(".ps-scrollbar-y-rail").remove();
-		$(".ps-container").removeClass("ps-container");
+		$(".cart_items_block .ps-scrollbar-y-rail").remove();
+		$(".cart_items_block .ps-container").removeClass("ps-container");
 	}
 
 	if (count==0){
 		$(".empty_cart").addClass("active");
 	}
 
-
-	$(".ps-container .ps-scrollbar-x-rail").remove();
+	$(".cart_items_block .ps-container .ps-scrollbar-x-rail").remove();
 }
 
 $(window).resize(function(){
@@ -61,6 +70,16 @@ $(document).on("focus", ".item_important", function(){
 	$(this).removeClass("error");
 });
 
+$(document).on("click", ".cart_help_button", function(){
+
+	if (!($(this).find(".help_block").hasClass("active"))) {
+		$(".help_block").removeClass("active");
+		$(this).find(".help_block").addClass("active");
+	}
+});
+
+
+
 
 $(document).on('click',".result_city span", function(){
 	$(".city").val($(this).text());
@@ -70,9 +89,21 @@ $(document).on('click',".result_city span", function(){
 });
 
 
+$(document).on('input','.select-city__main-input__sdec', function(){
+	$(".city").val($(this).val());
+
+});
+
+$(document).on("click", ".container_yandex_close", function(){
+	$(".alert_block.alert_cart").removeClass("active");
+});
 
 
 $(document).on('input','.city', function(){
+
+	$(".select-city__main-input__sdec").val($(this).val());
+
+
 	$(".result_city").find("span").remove();
 	$.ajax({ 
 		type: "POST", 
@@ -255,6 +286,22 @@ $(document).on('click',"#steps_controller-next_but div", function(){
 	
 });
 
+$(document).on("click", ".container_mobile button, .container_tablet button, .container_old button", function(){
+	$(".alert_block.alert_mobile, .alert_block.alert_tablet, .alert_block.alert_old ").removeClass("active");
+});
+
+$(document).ready(function(){
+	if (device.mobile() || device.tablet()) {
+		if (device.portrait()) {
+			$(".alert_block.alert_mobile").addClass("active");
+		}
+
+		if (device.landscape()) {
+			$(".alert_block.alert_tablet").addClass("active");
+		}
+	}
+});
+
 
 $(document).ready(function(){
 	reset_cost_total();
@@ -274,6 +321,9 @@ $(document).ready(function(){
 	 $('input,textarea').blur(function(){
 	   $(this).attr('placeholder',$(this).data('placeholder'));
 	 });
+
+
+	
 });
 
 function reset_cost_total() {
@@ -365,6 +415,18 @@ jQuery(function($){
 	});
 });
 
+jQuery(function($){
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".cart_help_button"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		   && (div.has(e.target).length === 0)
+			) {
+			$(".help_block").removeClass("active");
+			
+		}
+	});
+});
+
 
 function to_down_of_page() {
 	var browser_height = $(window).height();
@@ -383,3 +445,170 @@ jQuery(function($){
 	});
 });
 
+
+
+function get_city_list_sdec() {
+
+}
+
+
+
+function get_city_sdec() {
+	$.ajax({
+	  type: "POST",
+	  dataType: "json",
+	  url: "cart/get_city_sdec",
+	  success: function(data){
+	  	sdec_points=data;
+	  	myGeoObjects = [];
+	  	myMap.geoObjects.removeAll();
+		for (var i = 0; i < sdec_points.length; i++) {
+			console.log(sdec_points[i]);
+
+			var table = '<table><tbody>';
+
+			table += '<tr><td>Адрес:</td><td>'+ucfirst(sdec_points[i]["@attributes"]["Address"].toLowerCase())+'</td><tr>';
+			table += '<tr><td>Рабочее время</td><td>'+ucfirst(sdec_points[i]["@attributes"]["WorkTime"].toLowerCase())+'</td><tr>';
+
+			if ((sdec_points[i]["@attributes"]["Phone"]!=undefined) && (sdec_points[i]["@attributes"]["Phone"]!="")) {
+				table += '<tr><td>Телефон</td><td>'+sdec_points[i]["@attributes"]["Phone"]+'</td><tr>';	
+			}
+
+
+			if ((sdec_points[i]["@attributes"]["Note"]!=undefined) && (sdec_points[i]["@attributes"]["Note"]!="")) {
+				table += '<tr><td>Примечание</td><td>'+sdec_points[i]["@attributes"]["Note"]+'</td><tr>';	
+			}
+			   table += '</tbody></table>';
+
+			
+			table += "<button ";
+
+			if ((sdec_points[i]["@attributes"]["Address"]!=undefined) && (sdec_points[i]["@attributes"]["Address"]!="")) {
+					table += "data-Address='"+sdec_points[i]["@attributes"]["Address"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["City"]!=undefined) && (sdec_points[i]["@attributes"]["City"]!="")) {
+				table += "data-City='"+sdec_points[i]["@attributes"]["City"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["CityCode"]!=undefined) && (sdec_points[i]["@attributes"]["CityCode"]!="")) {
+				table += "data-CityCode='"+sdec_points[i]["@attributes"]["CityCode"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["Code"]!=undefined) && (sdec_points[i]["@attributes"]["Code"]!="")) {
+				table += "data-Code='"+sdec_points[i]["@attributes"]["Code"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["Name"]!=undefined) && (sdec_points[i]["@attributes"]["Name"]!="")) {
+				table += "data-Name='"+sdec_points[i]["@attributes"]["Name"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["Note"]!=undefined) && (sdec_points[i]["@attributes"]["Note"]!="")) {
+				table += "data-Note='"+sdec_points[i]["@attributes"]["Note"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["Phone"]!=undefined) && (sdec_points[i]["@attributes"]["Phone"]!="")) {
+				table += "data-Phone='"+sdec_points[i]["@attributes"]["Phone"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["WorkTime"]!=undefined) && (sdec_points[i]["@attributes"]["WorkTime"]!="")) {
+				table += "data-WorkTime='"+sdec_points[i]["@attributes"]["WorkTime"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["coordX"]!=undefined) && (sdec_points[i]["@attributes"]["coordX"]!="")) {
+				table += "data-coordX='"+sdec_points[i]["@attributes"]["WorkTime"]+"' ";
+			}
+
+			if ((sdec_points[i]["@attributes"]["coordY"]!=undefined) && (sdec_points[i]["@attributes"]["coordY"]!="")) {
+				table += "data-coordY='"+sdec_points[i]["@attributes"]["WorkTime"]+"' ";
+			}
+
+
+			table += " >Выбрать</button>";
+
+
+         
+			var object =  new ymaps.Placemark(
+					[sdec_points[i]["@attributes"]["coordY"], sdec_points[i]["@attributes"]["coordX"]], {
+						 balloonContentHeader: sdec_points[i]["@attributes"]["Name"],
+               			 balloonContent: table
+
+					}
+				);
+
+			myGeoObjects.push(object);
+			
+		};
+
+
+		clusterer = new ymaps.Clusterer();
+		clusterer.add(myGeoObjects);
+		myMap.geoObjects.add(clusterer);
+		myMap.setBounds(clusterer.getBounds());
+	  }
+  	});
+}
+
+var sdec_data = {
+	Address: "",
+	City: "",
+	CityCode: "",
+	Code: "",
+	Name: "",
+	Note: "",
+	Phone: "",
+	WorkTime: "",
+	coordX: "",
+	coordY: ""
+}
+
+
+$(document).on("click", "ymaps button" ,function(){
+	sdec_data.Address = $(this).data("address");
+	sdec_data.City = $(this).data("city");
+	sdec_data.CityCode = $(this).data("citycode");
+	sdec_data.Code = $(this).data("code");
+	sdec_data.Name = $(this).data("name");
+	sdec_data.Note = $(this).data("note");
+	sdec_data.Phone = $(this).data("phone");
+	sdec_data.WorkTime = $(this).data("worktime");
+	sdec_data.coordX = $(this).data("coordx");
+	sdec_data.coordY = $(this).data("coordy");
+
+	$(".alert_block.alert_cart").removeClass("active");
+});
+
+
+/*map*/
+
+var myMap;
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init () {
+    // Создание экземпляра карты и его привязка к контейнеру с
+    // заданным id ("map").
+    myMap = new ymaps.Map('map', {
+        // При инициализации карты обязательно нужно указать
+        // её центр и коэффициент масштабирования.
+        center: [55.76, 37.64], // Москва
+        zoom: 10,
+    }, {
+        
+    });
+
+    myMap.controls.add(
+   		new ymaps.control.ZoomControl()
+	);
+}
+
+function ucfirst(str) 
+{
+    var first = str.charAt(0).toUpperCase();
+    return first + str.substr(1);
+}
+
+$(window).load(function(){
+	get_city_sdec();
+});
